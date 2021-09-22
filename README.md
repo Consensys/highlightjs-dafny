6c590aca4dc9f0a73ba87309be532e5e6ec66160
Roberto Saltini <saltini.roberto@gmail.com>
2021-09-22 15:55:36 +1000

    Initial commit

---
+++ README.md
@@ -0,0 +1,100 @@
+# highlightjs-dafny
+
+[Dafny](https://github.com/dafny-lang/dafny) language grammar definition for [highlight.js](https://highlightjs.org/).
+
+## Usage
+Adapted from the ["Usage" section of highlightjs-cypher](https://github.com/highlightjs/highlightjs-cypher#usage).
+
+
+Simply include the Highlight.js library in your webpage or Node app, then load this module.
+
+### Static website or simple usage
+
+Simply load the module after loading Highlight.js. You'll use the minified version found in the `dist` directory. This module is just a CDN build of the language, so it will register itself as the Javascript is loaded.
+
+```html
+<script type="text/javascript" src="/path/to/highlight.min.js"></script>
+<script type="text/javascript" charset="UTF-8"
+  src="/path/to/highlightjs-dfny/dist/dafny.min.js"></script>
+<script type="text/javascript">
+  hljs.initHighlightingOnLoad();
+</script>
+```
+
+<!-- ### Using directly from the UNPKG CDN
+
+```html
+<script type="text/javascript"
+  src="https://unpkg.com/highlightjs-dafny/dist/dafny.min.js"></script>
+```
+
+- More info: <https://unpkg.com>
+
+### With Node or another build system
+
+If you're using Node / Webpack / Rollup / Browserify, etc, simply require the language module, then register it with Highlight.js.
+
+```javascript
+var hljs = require('highlightjs');
+var hljsDafny = require('highlightjs-dafny');
+
+hljs.registerLanguage("dafny", hljsDafny);
+hljs.initHighlightingOnLoad();
+``` -->
+
+### React
+
+You need to import both Highlight.js and third-party language like Cypher:
+
+```js
+import React, {Component} from 'react'
+import 'highlight.js/scss/darcula.scss' # your favourite theme
+import dafny from './dafny'
+import hljs from 'highlight.js'
+hljs.registerLanguage('dafny', dafny);
+
+class Highlighter extends Component
+{
+  constructor(props)
+  {
+    super(props);
+    hljs.initHighlightingOnLoad();
+  }
+
+  render()
+  {
+    let {children} = this.props;
+    return
+    {
+      <pre ref={(node) => this.node = node}>
+        <code className="dafny">
+          {children}
+        </code>
+      </pre>
+    }
+  }
+}
+
+export default Highlighter;
+```
+
+## License
+
+Highlight.js is released under the CC0 1.0 License. See [LICENSE][1] file
+for details.
+
+### Author
+
+Roberto Saltini
+
+### Maintainer
+
+Roberto Saltini
+
+## Links
+
+- The official site for the Highlight.js library is <https://highlightjs.org/>.
+- The Highlight.js GitHub project: <https://github.com/highlightjs/highlight.js>
+- Dafny GitHub repo: <https://github.com/dafny-lang/dafny>
+
+[1]: https://github.com/highlightjs/highlightjs-cypher/blob/master/LICENSE
---
+++ src/dafny.js
@@ -0,0 +1,210 @@
+/**
+ * Copyright 2021, ConsenSys Software Inc.
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+ */
+
+export default function(hljs)     {
+    const IDENT_WITH_BRACKETS = /[a-zA-Z_<>]*/
+    
+    const DAFNY_KEYWORDS = [    
+        "abstract", 
+        "allocated", 
+        "as", 
+        "assert", 
+        "assume",
+        "break", 
+        "by" ,
+        "calc" , 
+        "case" , 
+        "class" , 
+        "codatatype" ,
+        "colemma" , 
+        "const" , 
+        "constructor" , 
+        "copredicate" ,
+        "datatype" , 
+        "decreases" ,
+        "else" , 
+        "ensures" , 
+        "exists" , 
+        "export" , 
+        "extends" ,
+        "forall" , 
+        "fresh" , 
+        "function" , 
+        "ghost" ,
+        "if" , , 
+        "import" ,  , 
+        "include" , 
+        "inductive" ,
+        "invariant" , 
+        "is" ,  
+        "iterator" ,
+        "label" , 
+        "lemma" , 
+        "match" , 
+        "method" ,
+        "modifies" , 
+        "modify" , 
+        "module" , 
+        "nameonly" , 
+        "new" , 
+        "newtype" , 
+        "old" , 
+        "opened" , 
+        "predicate" , 
+        "print" , 
+        "provides" ,
+        "reads" , 
+        "real" , 
+        "refines" , 
+        "requires" , 
+        "return" ,
+        "returns" , 
+        "reveal" , 
+        "reveals" ,
+        "static" , 
+        "string" ,
+        "then" , 
+        "this" , 
+        "trait" , 
+        "twostate" , 
+        "type" ,
+        "unchanged" , 
+        "var" , 
+        "while" , 
+        "witness" ,
+        "yield" , 
+        "yields" , 
+    ];
+
+    const DAFNY_TYPES = [
+        "bool",         
+        "char" , 
+        "imap" ,
+        "iset" ,   
+        "int" , 
+        "map" ,  
+        "multiset" ,        
+        "nat" ,     
+        "object" , 
+        "object?" ,    
+        "ORDINAL",     
+        "seq" , 
+        "set" ,   
+        "array", 
+        "bc"                                                      
+    ];
+    const DAFNY_LITERALS = [
+        "false" , 
+        "null" ,  
+        "true"               
+    ];
+    const KEYWORDS = {
+        keyword: DAFNY_KEYWORDS,
+        type: DAFNY_TYPES,
+        literal: DAFNY_LITERALS
+    };
+    const TYPE_FOR_IDENTIFIERS = {
+                begin: new RegExp(
+                    /(?<=[^:]:[\s\n]*)/.source +
+                    IDENT_WITH_BRACKETS.source
+                ),
+                scope: 'type'
+            };
+    
+    const OPERATORS = {
+        begin: /(?<!\w)/.source +
+            "(" + 
+            [
+            "&&", 
+            "\\|\\|",
+            "<==>",
+            "==>",
+            "<==",
+            "==",
+            "!=",
+            "!",
+            ">",
+            "<",
+            ">=",
+            "<=",
+            "\\+",
+            "-",
+            "\\*",
+            "/",
+            "%",
+            ">>",
+            "<<",
+            "&",
+            "\\|",
+            "\\^",
+            "!!",
+            "in",
+            "!in",
+            ":=",
+            ":\\|",
+            "::",
+            "="
+        ].join("|") + 
+        ")" + 
+        /(?!\w)/.source ,
+        scope: 'operator',
+
+    };
+    return {
+        case_insensitive: false,
+        keywords: KEYWORDS,
+        contains: [
+            hljs.QUOTE_STRING_MODE,
+            hljs.C_LINE_COMMENT_MODE,
+            hljs.NUMBER_MODE,
+            hljs.COMMENT(
+                '/\\*', // begin
+                '\\*/', // end
+                {
+                    contains: [
+                    {
+                        scope: 'doctag', begin: '@\\w+'
+                    }
+                    ]
+                }
+            ),
+            TYPE_FOR_IDENTIFIERS,
+            OPERATORS,
+            {
+                begin: "\\b(" + DAFNY_TYPES.join("|") + ")\\b" + IDENT_WITH_BRACKETS.source,
+                scope: "type"
+            },
+            {
+                begin: ["(?:" + hljs.IDENT_RE + "\\s+)", IDENT_WITH_BRACKETS, /\s*(?=\()/], 
+                scope: {
+                    2: "title.function"
+                }, 
+                keywords: KEYWORDS, 
+                contains: [{
+                    className: "params", 
+                    begin: /\(/,
+                    end: /\)/, 
+                    keywords: KEYWORDS, 
+                    relevance: 0,
+                    contains: [hljs.QUOTE_STRING_MODE, hljs.C_BLOCK_COMMENT_MODE, TYPE_FOR_IDENTIFIERS, OPERATORS]
+                }, 
+                hljs.C_LINE_COMMENT_MODE, 
+                hljs.C_BLOCK_COMMENT_MODE]
+            }    
+        ]
+    };
+
+}
